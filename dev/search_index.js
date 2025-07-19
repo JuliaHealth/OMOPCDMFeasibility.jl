ea2aa5c64bad11f2e3112c6292b59a5e10b6c3f8
Documenter.jl <documenter@juliadocs.github.io>
2025-07-19 10:22:26 +0000

    build based on 51bc72f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OMOPCDMFeasibility","page":"Home","title":"OMOPCDMFeasibility","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OMOPCDMFeasibility.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
